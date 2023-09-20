fab3768c882eb6bcc348f5e72672c8c77630ae92
Danick Takam <danick.takam@datnek.be>
2023-09-20 21:39:33 +0200

    feat: Authorize null to mediaId in Beer

--- Brewery.BO/Entities/BeerEntity.cs
+++ Brewery.BO/Entities/BeerEntity.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 using Elia.Core.BaseModel;
 
 namespace Brewery.BO.Entities;
@@ -55,7 +56,7 @@ public class BeerEntity: Track
     /// <summary>
     /// 
     /// </summary>
-    public Guid MediaId { get; set; }
+    public Guid?  MediaId { get; set; }
     
     /// <summary>
     /// 
